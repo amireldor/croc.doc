adeb10db856430f5c2791dd6604e39e5234a1439
Amir Eldor <amir.eldor@gmail.com>
2016-08-06 16:28:41 +0300

    Some directory structuring and index.html and gulp tasks

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -10,21 +10,41 @@ const sourcemaps = require('gulp-sourcemaps');
 
 // Do watchify/browiserfy magic
 const bundler = watchify(browserify(
-    Object.assign({}, watchify.args, {entries:  ['./src/main.js']})
+    Object.assign({}, watchify.args, {entries:  ['./src/js/main.js']})
 ));
 
 function bundle() {
     return bundler.bundle()
         .on('error', gutil.log.bind(gutil, 'Browserify error'))
-        .pipe(source('crocfarm.js'))
+        .pipe(source('static/crocfarm.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest('./dev-build'));
 }
 gulp.task('js', bundle);
 bundler.on('update', bundle);
 bundler.on('log', gutil.log);
+
+// stuff
+// ...
+
+const indexHtmlPath = './src/index.html';
+
+// more tasks
+gulp.task('copy-html-dev', function () {
+    gulp.src(indexHtmlPath).pipe(gulp.dest('./dev-build'));
+
+});
+
+gulp.task('copy-html-build', function () {
+    gulp.src(indexHtmlPath).pipe(gulp.dest('./build'));
+});
+
+gulp.task('scss', function () {
+});
+
+gulp.task('dev', ['copy-html-dev', 'scss', 'js']);
 
 gulp.task('default', function () {
     console.log('Hi.');
