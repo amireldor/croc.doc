48d6bcd28879e64d427527358e5ee86e657f4aa5
Amir Eldor <amir.eldor@gmail.com>
2016-08-06 18:18:09 +0300

    uhuhuhuh ugly layout design i hate everything commit bye

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -7,6 +7,9 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
 
 // Do watchify/browiserfy magic
 const bundler = watchify(browserify(
@@ -20,7 +23,7 @@ function bundle() {
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dev-build'));
+        .pipe(gulp.dest('./build'));
 }
 gulp.task('js', bundle);
 bundler.on('update', bundle);
@@ -30,21 +33,51 @@ bundler.on('log', gutil.log);
 // ...
 
 const indexHtmlPath = './src/index.html';
+const scssPath = './src/scss/**/*.scss';
 
 // more tasks
-gulp.task('copy-html-dev', function () {
-    gulp.src(indexHtmlPath).pipe(gulp.dest('./dev-build'));
 
+// Delete build folder
+gulp.task('clean', function () {
+    return del('./build/**/*');    
 });
 
-gulp.task('copy-html-build', function () {
+gulp.task('copy-html', function () {
     gulp.src(indexHtmlPath).pipe(gulp.dest('./build'));
 });
 
-gulp.task('scss', function () {
+gulp.task('html-watch', function () {
+    return gulp
+    .watch(indexHtmlPath, ['copy-html'])
+    .on('change', function (event) {
+        console.log('I think HTML changed: ', event.path, event.type);
+    });
 });
 
-gulp.task('dev', ['copy-html-dev', 'scss', 'js']);
+gulp.task('sass', function () {
+    const sassOptions = {
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+    };
+    return gulp
+        .src(scssPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./build/static'));
+
+});
+
+gulp.task('sass-watch', function () {
+    return gulp
+        .watch(scssPath, ['sass'])
+        .on('change', function (event) {
+            console.log(event.path + ' was ' + event.type);
+        });
+});
+
+gulp.task('dev', ['clean', 'copy-html', 'html-watch', 'sass', 'sass-watch', 'js']);
 
 gulp.task('default', function () {
     console.log('Hi.');
