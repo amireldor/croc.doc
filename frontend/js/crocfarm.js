4dabfc3b29e5a1a81b3a257deab9f83ed2f5bcc3
Amir Eldor <amir.eldor@gmail.com>
2016-08-26 13:01:08 +0300

    Showing the json from the test, nothing really testing anything

--- frontend/js/crocfarm.js
+++ frontend/js/crocfarm.js
@@ -1,6 +1,5 @@
 'use strict';
 
 exports.feedCroc = function (doc) {
-    console.log('denmark', doc);
-    return (new Promise(resolve => resolve()));
+    return fetch('/feedcroc', {})
 }
--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,25 +1,26 @@
 let expect = chai.expect;
+const crocfarm = require('../js/crocfarm');
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('Should return -1 when value is not present', function() {
+describe('Array',  function () {
+    describe('#indexOf()',  function () {
+        it('Should return -1 when value is not present',  function () {
             expect([1, 2, 3].indexOf(4)).equal(-1);
         });
     });
 });
 
-describe('Documents and stuff', function() {
-    describe('Posting new documents', function() {
+describe('Documents and stuff',  function () {
+    describe('Posting new documents',  function () {
 
         beforeEach(function () {
             sinon.stub(window, 'fetch');
-            let res = new window.Response('{"status": "ok", "name": "boarish-mosquito"}', {
+            let response = new window.Response('{"status": "ok", "name": "boarish-mosquito"}', {
                 status: 200,
                 headers: {
-                    'Content-type': 'application/json'
+                    'Content-Type': 'application/json'
                 }
             });
-            window.fetch.returns(Promise.resolve(res));
+            window.fetch.returns(Promise.resolve(response));
         });
 
         afterEach(function () {
@@ -27,7 +28,11 @@ describe('Documents and stuff', function() {
         });
 
         it('should parse the json from the backend', sinon.test(function () {
-            return crocfarm.feedCroc('tasty deer');
+            return crocfarm.feedCroc('tasty deer').then(function (result) {
+                return result.json();
+            }).then(function (json) {
+                console.log('RESULT', json);
+            });
         }));
 
         it('should fail nicely if server doesn\'t like it', sinon.test(function () {
