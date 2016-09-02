3d6705e5c4d06c40f351225132502576d8c6ac3f
Amir Eldor <amir.eldor@gmail.com>
2016-09-02 21:28:13 +0300

    Disable/enable feed button to prevent multiple submits

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,33 +5,34 @@ const crocfarm = require('./crocfarm');
 const docForm = document.getElementById('doc-form');
 const docText = document.getElementById('doc-text');
 const crocLine = document.getElementById('croc-line');
+const feedButton = document.getElementById('feed-button');
 
 docForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('WeeE', docText.value);
 
-    const request = {
-        doc: docText.value
-    };
-    const headers = {
-        'Content-Type': 'application/json'
-    };
-
+    disableFeeder();
     crocfarm.feedCroc(doc).then(function (response) {
+        enableFeeder();
         return response.json();
     }).then(function (json) {
-        // Show NICE stuff in the GUI
         if (json.status == 'ok') {
             updateCrocLine(json);
         } else {
             displayError(json);
         }
     }).catch(function (error){
-        // Show BAD stuff in the GUI
+        enableFeeder();
         console.error(error);
     });
-
 });
+
+function disableFeeder() {
+    feedButton.disabled = 'disabled';
+}
+
+function enableFeeder() {
+    feedButton.disabled = '';
+}
 
 function updateCrocLine(json) {
     const name = crocLine.querySelector('.name');
