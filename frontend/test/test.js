9e30c02b630cf734296e883f9539a63671f020fe
Amir Eldor <amir.eldor@gmail.com>
2016-09-02 21:03:12 +0300

    Useless rearranging of test.js

--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,14 +1,6 @@
 const demand = require('must');
 const crocfarm = require('../js/crocfarm');
 
-describe('Array',  function () {
-    describe('#indexOf()',  function () {
-        it('Should return -1 when value is not present',  function () {
-            demand([1, 2, 3].indexOf(4)).equal(-1);
-        });
-    });
-});
-
 describe('Documents and stuff',  function () {
     describe('Posting new documents',  function () {
 
@@ -16,16 +8,6 @@ describe('Documents and stuff',  function () {
             if (window.__html__) {
                 document.body.innerHTML = window.__html__['test/index.html'];
             }
-
-            sinon.stub(window, 'fetch');
-            let response = new window.Response('{"status": "ok", "name": "boarish-mosquito"}', {
-                status: 200,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            window.fetch.returns(Promise.resolve(response));
-
         });
 
         afterEach(function () {
@@ -33,20 +15,25 @@ describe('Documents and stuff',  function () {
         });
 
         it('must parse the json from the backend', sinon.test(function (done) {
-            demand(document.getElementById('doc-text')).must.not.be.undefined();
-            crocfarm.feedCroc('tasty deer').then(function (result) {
+            // Stub a good response
+            sinon.stub(window, 'fetch');
+            const response = new window.Response('{"status": "ok", "name": "boarish-mosquito"}', {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            window.fetch.returns(Promise.resolve(response));
+
+            crocfarm.feedCroc('I like chinese').then(function (result) {
                 return result.json();
             }).then(function (json) {
                 demand(json.status).not.undefined();
                 demand(json.name).not.undefined();
-                //demand(json.dog).not.undefined();
                 done();
             }).catch(function (error) {
                 done(error);
             });
-        }));
-
-        it('must fail nicely if server doesn\'t like it', sinon.test(function () {
         }));
     });
 });
